7987da17a2a605dac00d0c66f295f4bdea28e75d
Ivan Mitrevski <mitrevski.ivan@hotmail.com>
2021-03-18 00:32:57 +0100

    Naive/Not dynamic filtering of items

--- app.js
+++ app.js
@@ -71,12 +71,45 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  }
 ];
 
+//Get the first element in the document with class="section-center"
 const sectionCenter = document.querySelector('.section-center');
 
+//Get all elements in the document with class="filter-btn"
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+//load items
 window.addEventListener('DOMContentLoaded', () => {
   displayMenuItems(menu);
+});
+
+// filter items
+filterBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const category = e.currentTarget.dataset.category;
+
+    //filtered out array called menuCategory
+    const menuCategory = menu.filter((menuItem) => {
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+
+    if (category === "all") {
+      displayMenuItems(menu)
+    } else {
+      displayMenuItems(menuCategory)
+    }
+  });
 });
 
 function displayMenuItems(menuItems) {
